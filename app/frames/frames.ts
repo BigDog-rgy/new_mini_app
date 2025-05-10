4d9a05d16739f93d9b68e88507558d0ba45794b8
C-DOG <cvrolson@gmail.com>
2025-05-10 14:44:47 -0500

    revised route and frames

--- app/frames/frames.ts
+++ app/frames/frames.ts
@@ -1,14 +1,12 @@
 import { createFrames } from "frames.js/next";
 
 export type State = {
-  step: "meal" | "side" | "drink" | "done";
-  meal?: string;
-  side?: string;
-  drink?: string;
+  step: "meal" | "side" | "drink";
 };
 
 export const frames = createFrames<State>({
   initialState: {
     step: "meal",
   },
+  basePath: "/frames",
 });
